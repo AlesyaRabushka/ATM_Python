ebfc7a8ee04f7b813b1b320f2f7265314999a007
lsklemm <73190190+lsklemm@users.noreply.github.com>
2022-03-10 11:28:30 +0300

    banknota

--- menuoperations.py
+++ menuoperations.py
@@ -30,13 +30,44 @@ class MenuOperations(Bankomat):
             # данные о карточке
             if k == 1:
                 card.print_card_info()
-                single_t.log('Данные банковской карты', True,'')
+                single_t.log('Данные банковской карты', True)
 
             # выдача наличных
             elif k == 2:
-                money = int(input("Введите сумму выдачи: "))
-                give_money = GiveMoney()
-                give_money.money_out(card, money, storage, single_t)
+                print("\tВыберите нужную сумму:")
+                print("\t1 - 5")
+                print("\t2 - 10")
+                print("\t3 - 20")
+                print("\t4 - 50")
+                print("\t5 - 100")
+                print("\t6 - Другая сумма")
+                try:
+                    m = int(input())
+                    if m == 6:
+                         money = int(input("Введите сумму выдачи: "))
+                         give_money = GiveMoney()
+                         give_money.money_out(card, money, storage, single_t)
+                    elif m == 1:
+                        give_money = GiveMoney()
+                        give_money.money_out(card, 5, storage, single_t)
+                    elif m == 2:
+                        give_money = GiveMoney()
+                        give_money.money_out(card, 10, storage, single_t)
+                    elif m == 3:
+                        give_money = GiveMoney()
+                        give_money.money_out(card, 20, storage, single_t)
+                    elif m == 4:
+                        give_money = GiveMoney()
+                        give_money.money_out(card, 50, storage, single_t)
+                    elif m == 5:
+                        give_money = GiveMoney()
+                        give_money.money_out(card, 100, storage, single_t)
+                    else:
+                        print("\tНеверный код операции\n")
+                except ValueError:
+                    print("\tНеверный код операции")
+
+
 
             # оплата телефона
             elif k == 3:
@@ -48,7 +79,7 @@ class MenuOperations(Bankomat):
                     telephone.telephone_pay(card, money, tel, storage, single_t)
                 else:
                     print('\tНеверный номер операции. Повторите попытку позже.\n')
-                    single_t.log('Пополнение счета телефона', False,' Неверный номер операции')
+                    single_t.log('Пополнение счета телефона', False)
 
             # смена пин-код
             elif k == 4:
@@ -62,7 +93,7 @@ class MenuOperations(Bankomat):
 
             # выход из проги
             elif k == 0:
-                single_t.log('Выход из системы', True,'')
+                single_t.log('Выход из системы', True)
                 exit()
             else:
                 print('Неверный номер операции')
