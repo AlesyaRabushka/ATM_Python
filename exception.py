1107031f0d805d5e005bbb1943a960d827a67472
repLLEH <73431596+repLLEH@users.noreply.github.com>
2022-03-14 20:23:06 +0300

    Exception

--- exception.py
+++ exception.py
@@ -1,9 +1,14 @@
-from singleton import Singleton
-class MyException(Singleton):
+class pinValueException(Exception):
     """класс для генерации исключений"""
     def __init__(self, error):
-        self.error_type = error
-
+        self.error_pin = error
     #возвращает строку с названием ошибки
     def __str__(self):
-        return f'{self.error_type}'
+        return f'{self.error_pin} пин-код должен содержать только цифры'
+
+class pinLengthException(Exception):
+    def __init__(self, pin):
+        self.error = pin
+        # возвращает строку с названием ошибки
+    def __str__(self):
+        return f'{self.error} пин-код должен содержать 4 символа'
